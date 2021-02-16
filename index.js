1cbfd275228c41c55362229b1490f79aba30a56c
Cris Lom <bigc4997@gmail.com>
2021-02-15 22:56:40 -0500

    mobile responsive

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 (() => {
     const crawlData = data;
-    // console.log(crawlData);
 
-    //
     let i = 0;
     while (i < crawlData.length) {
         const { date, title, showAnchor, bookAnchor, bookImageSrc } = crawlData[i];
@@ -12,7 +10,6 @@
 
         const showDate = document.createElement('p');
         showDate.innerHTML = `Interviewed on ${date}`;
-        // showDate.style.fontWeight = 'bold';
 
         const showLink = document.createElement('a');
         showLink.innerHTML = 'Playlist Link';
@@ -21,51 +18,47 @@
         const bookLink = document.createElement('a');
         bookLink.innerHTML = 'Book Link';
         bookLink.href = bookAnchor;
-        // bookLink.style.fontSize = '1.5em';
 
-        const bookLinkClone = bookLink.cloneNode();
+        const bookImageLink = bookLink.cloneNode();
 
         const bookImg = document.createElement('img');
-        bookImg.style.width = '160px';
-        bookImg.style.boxShadow = '0 10px 5px -5px grey';
+        bookImg.classList.add('bookImg');
         bookImg.src = bookImageSrc;
 
-        bookLinkClone.appendChild(bookImg);
+        bookImageLink.appendChild(bookImg);
 
-        const showInfo = document.createElement('div');
 
         const bookEntry = document.createElement('div');
-        bookEntry.style.display = 'flex';
-        bookEntry.style.alignItems = 'center';
-        bookEntry.style.width = '50%';
+        bookEntry.classList.add('bookEntry');
 
-        const bookInfo = document.createElement('div');
-
-        bookInfo.append(bookLinkClone);
-        bookInfo.style.padding = '2em 0';
-        showInfo.style.padding = '2em';
+        const showInfo = document.createElement('div');
+        showInfo.classList.add('showInfo');
+        // showInfo.style.padding = '2em';
 
         showInfo.append(showTitle);
         showInfo.append(bookLink);
         showInfo.append(showDate);
         showInfo.append(showLink);
 
-        bookEntry.append(bookInfo);
+        bookEntry.append(bookImageLink);
         bookEntry.append(showInfo);
-        // bookEntry.style.outline = '1px solid black';
 
         const bookList = document.getElementById('booklist');
         bookList.append(bookEntry);
-        bookList.style.display = 'flex';
-        bookList.style.flexWrap = 'wrap';
-        // bookList.style.alignItems = 'center';
-        bookList.style.margin = 'auto';
-
-        bookList.style.maxWidth = '1000px';
-
-        // document.getElementById('booklist').append(bookSite);
-        // document.getElementById('booklist').append(bookImage);
 
         i += 1;
     }
+
+    const checkWindowSize = () => {
+        console.log(window.innerWidth);
+        if (window.innerWidth < 720)
+            document.getElementById('booklist').classList.add('mobile');
+        else
+            document.getElementById('booklist').classList.remove('mobile');
+    };
+
+    window.onresize = () => checkWindowSize();
+    window.onload = () => checkWindowSize();
+
+    // window.onload = () => document.getElementById('booklist').classList.add('mobile');
 })();
