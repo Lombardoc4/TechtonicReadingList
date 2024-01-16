ba29d58635d5fc70650cf0cfc1d699450ab95592
cris lombardo <bigc4997@gmail.com>
2024-01-16 13:09:59 -0500

    commenting and localStorage saving recent searches

--- index.js
+++ index.js
@@ -1,57 +1,76 @@
 ((data) => {
     const qs = (selector, parent = document) => parent.querySelector(selector);
     const qsa = (selector, parent = document) => parent.querySelectorAll(selector);
-    const createEl = el => document.createElement(el);
-    const formatDate = (date) => {
-        const yyyy = new Date(date).getFullYear();
-        const mm = new Date(date).getMonth() + 1;
-        const dd = new Date(date).getDay();
+    const createEl = (el) => document.createElement(el);
+    const bookList = qs("#booklist");
+    const imgPlaceholder = qs(".img-placeholder");
+    const bookPlaceholder = qs(".book-entry-placeholder");
+    const searchResultsContainer = qs(".search-results-container");
+    const searchInput = qs("#searchInput")
 
-        if ([mm, dd, yyyy].includes('NaN')) return '';
+    const ls = {
+        key: 'techtonic-recents',
+        max: 5
+    }
 
-        return `${mm}-${dd}-${yyyy}`;
+    // const parseLength = 50;
+    // const parseTitle = copy => (copy.length <= parseLength ? copy : `${copy.slice(0, parseLength)}...`);
+
+    const scrollToOffset = (el, offset) => {
+        const { top: elTop } = el.getBoundingClientRect();
+        if (elTop !== offset) {
+            window.scrollTo({ top: elTop + window.scrollY - offset, behavior: "smooth" });
+        }
     };
-    const bookList = qs('#booklist');
-    const imgPlaceholder = qs('.img-placeholder');
-    const bookPlaceholder = qs('.book-entry-placeholder');
-    const parseLength = 50;
-    const parseTitle = copy => (copy.length <= parseLength ? copy : `${copy.slice(0, parseLength)}...`);
 
+    const handleLocalStorage = (value) => {
+        const recents = window.localStorage.getItem(ls.key);
+        let dataArr = recents ? JSON.parse(recents) : [];
+
+        // * Only retain a certain number of items as recent
+        if (dataArr.length > ls.max) {
+            dataArr.pop();
+        }
+
+        dataArr.unshift(value);
+        window.localStorage.setItem(ls.key, JSON.stringify(dataArr))
+    }
 
     const loadCard = async ({ date, title, showAnchor, bookSrc }, i) => {
-            const bookTemplate = bookPlaceholder.cloneNode(true)
-            bookTemplate.dataset.index = i;
-            bookTemplate.classList.remove('book-entry-placeholder', 'd-none')
+        const bookTemplate = bookPlaceholder.cloneNode(true);
+        bookTemplate.dataset.index = i;
+        bookTemplate.classList.remove("book-entry-placeholder", "d-none");
 
-            // Update Show Date
-            qs('.show-data', bookTemplate).innerHTML = date;
+        // Update Show Date
+        qs(".show-data", bookTemplate).innerHTML = date;
 
-            // Update Title
-            qs('.show-title', bookTemplate).innerHTML = title;
+        // Update Title
+        qs(".show-title", bookTemplate).innerHTML = title;
 
-            // Add title attr to view full show title
-            qs('.book-container', bookTemplate).title = title;
+        // Target attribute when scrolling to book from search results
+        // Add title attr to view full show title
+        qs(".book-container", bookTemplate).title = title;
 
-            // Update button anchors
-            const [book, show] = qsa('.btn-group .btn', bookTemplate);
-            book.href = bookSrc;
-            show.href = showAnchor;
+        // Update button anchors
+        const [book, show] = qsa(".btn-group .btn", bookTemplate);
+        book.href = bookSrc;
+        show.href = showAnchor;
 
-            // Add to DOM
-            bookList.append(bookTemplate);
+        // Add to DOM
+        bookList.append(bookTemplate);
     };
 
     const lazyAddImage = () => {
-        const unloadedBooks = qsa('.book-entry.lazy');
+        const unloadedBooks = qsa(".book-entry.lazy");
 
         for (let i = 0; i < unloadedBooks.length; i++) {
-            const book = unloadedBooks[i]
-            const bookData = data[book.dataset.index]
+            const book = unloadedBooks[i];
+            const bookData = data[book.dataset.index];
             const { top: elTop } = book.getBoundingClientRect();
 
             // if element out of view skip
             if (elTop > window.innerHeight) {
-                return
+                return;
             }
 
             // Clone Template
@@ -59,115 +78,132 @@
 
             // Update outer anchor
             imgTemplate.href = bookData.bookSrc;
-            imgTemplate.classList.remove('img-placeholder', 'd-none');
-
+            imgTemplate.classList.remove("img-placeholder", "d-none");
 
             // Update inner img
-            const bookImg = qs('img', imgTemplate);
+            const bookImg = qs("img", imgTemplate);
             bookImg.alt = bookData.title;
             bookImg.src = bookData.bookImageSrc;
             bookImg.onload = () => {
-                bookImg.style.height = 'auto';
-                imgTemplate.classList.remove('preloader');
+                bookImg.style.height = "auto";
+                imgTemplate.classList.remove("preloader");
             };
 
             // Add to DOM
-            qs('.book-container', book).prepend(imgTemplate);
-            book.classList.remove('lazy');
+            qs(".book-container", book).prepend(imgTemplate);
+            book.classList.remove("lazy");
         }
     };
 
 
-    const unFreezeScroll = () => {
-        const scrollY = document.body.style.top;
-        document.body.style.position = '';
-        document.body.style.top = '';
-        document.body.style.left = '';
-        document.body.style.right = '';
-        window.scrollTo(0, parseInt(scrollY || '0') * -1);
-    }
+    const closeSearchResults = () => {
+        // Resume scrolling
+        document.body.classList.remove("noscroll");
 
-    const filterContainer = qs('.filter-container');
-    filterContainer.addEventListener('click', function(e)  {
-        if (e.target === this){
-            this.classList.remove('open');
-        }
+        // Hide Results
+        searchResultsContainer.classList.remove("open");
+    };
+
+    const openSearchResults = () => {
+        // Freeze body scrolling
+        document.body.classList.add("noscroll");
+        // Show Results
+        searchResultsContainer.classList.add("open");
+    };
+
+    searchResultsContainer.addEventListener("click", function (e) {
+        e.target === this && closeSearchResults();
+    });
+
+    // * OnFocus scroll search bar to top
+    searchInput.addEventListener("focus", (e) => {
+        // Scroll searchbar to top
+        scrollToOffset(qs(".search-bar"), 32);
+
+        // TODO: Below
+        // if (recently-searched) {
+            // openSearchResults()
+            // show recently-searched
+        // }
     })
 
-    qs('#searchInput').addEventListener('input', (e) => {
-        const input = e.target.value;
-        // const filterContainer = qs('.filter-container');
+    // On Input Reveal Results
+    searchInput.addEventListener("input", (e) => {
+        const input = e.target
+        const {value} = input;
 
-        if (input.length <= 0) {
-            // todo Hide list if visible
-            filterContainer.classList.remove('open')
+        // Scroll searchbar to top
+        scrollToOffset(qs(".search-bar"), 32);
 
-            unFreezeScroll();
+        // * Open Results Container
+        if (!document.body.classList.contains("noscroll")) openSearchResults();
 
+        // * Close container if input is empty
+        if (value.length <= 0) {
+            closeSearchResults();
+            return;
+        }
+
+        // * Filter through data
+        const searchResults = [
+            ...data.filter(
+                (item) =>
+                    new RegExp(value.toLowerCase(), "gi").exec(item.title.toString().toLowerCase())?.length || false
+            ),
+        ];
+
+        // * Reset Results to replace with new result
+        const searchResContainer = qs(".search-results");
+        searchResContainer.innerHTML = "";
+
+        // * Handle no search results
+        if (searchResults.length <= 0) {
+            const msg = createEl("h2");
+            msg.innerHTML = "No Results";
+            searchResContainer.append(msg);
 
             return;
         }
 
-        if (!filterContainer.classList.contains('open')) {
-            const scrollY = document.documentElement.style.getPropertyValue('--scroll-y');
-            document.body.style.position = 'fixed';
-            document.body.style.top = `-${scrollY}`;
-            document.body.style.left = 0;
-            document.body.style.right = 0;
+        // * Add Results to DOM
+        searchResults.map(({date, bookImageSrc, title}) => {
+            const resultTemplate = qs(".s-result").cloneNode(true);
+            resultTemplate.classList.remove("d-none");
+            resultTemplate.addEventListener("click", () => {
+                closeSearchResults();
 
-            filterContainer.classList.add('open');
-        }
-        // Loop through data
-        const searchResults = [...data.filter(item =>
-            new RegExp(input.toLowerCase(), 'gi').exec(item.title.toString().toLowerCase())?.length || false)
-        ].reverse();
+                input.value = title;
+                scrollToOffset(qs(`[title='${title}']`), 80);
 
 
-        const searchResContainer = qs('.search-results');
-        searchResContainer.innerHTML = '';
-
-        if (searchResults.length <= 0) {
-            const msg = createEl('h2');
-            msg.innerHTML = 'No Results';
-            searchResContainer.append(msg);
-
-        } else {
-            // Add Options
-            searchResults.map(res => {
-                const date =  formatDate(res.date);
-                const template = qs('.s-result').cloneNode(true);
-                template.classList.remove('d-none');
-                template.dataset.date = date;
-                qs('.result-date', template).innerHTML = date;
-                qs('img', template).src = res.bookImageSrc;
-                qs('.result-title', template).innerHTML = res.title;
-
-                template.addEventListener('click', () => {
-                    unFreezeScroll();
-                    filterContainer.classList.remove('open');
-
-
-                    qs(`[title='${res.title}']`).scrollIntoView();
-
-                })
-                searchResContainer.append(template);
+                // Add to Local Storage
+                handleLocalStorage(title)
             });
-        }
 
+            // Update template values
+            qs(".result-date", resultTemplate).innerHTML = date;
+            qs("img", resultTemplate).src = bookImageSrc;
+            qs(".result-title", resultTemplate).innerHTML = title;
 
-        searchResContainer.scrollTop = searchResContainer.scrollHeight;
+            // Add to DOM
+            searchResContainer.append(resultTemplate);
+
+        });
+
     });
 
-    window.addEventListener('load', async () => {
+    qs('.clear-search').addEventListener('click', () => {
+        searchInput.value = '';
+        searchInput.dispatchEvent(new Event('input', { bubbles: true }));
+    })
 
-        for (let i = 0; i < data.length; i++) {
-            loadCard(data[i], i);
-        }
+    window.addEventListener("load", async () => {
+        for (let i = 0; i < data.length; i++) loadCard(data[i], i);
         lazyAddImage();
     });
 
     window.onscroll = () => {
         lazyAddImage();
-        document.documentElement.style.setProperty('--scroll-y', `${window.scrollY}px`);
     };
+
 })(data);
